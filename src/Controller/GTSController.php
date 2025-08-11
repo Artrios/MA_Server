e70dbf5b46c9d1bf641302d555dacc47cb716acf
Artrios <dapaquelet@hotmail.com>
2025-08-11 15:46:33 +0800

    Create endpoints

--- src/Controller/GTSController.php
+++ src/Controller/GTSController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Service\MA_Helper;
+use App\Service\GTS_Helper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -20,7 +22,7 @@ class GTSController extends AbstractController
     }
 
     /**
-     * @Route("/worldexchange/info", name="ping_server")
+     * Ping the server to determine it is available
      */
     #[Route("/pokemonrse/worldexchange/info", name: "ping_server")]
     public function info()
@@ -32,12 +34,15 @@ class GTSController extends AbstractController
     }
 
     /**
-     * @Route("/worldexchange/result", name="gts_result")
+     * Checks if the players deposited pokemon in the GTS has been traded or not
+     * 0x0004 = The Pokemon has not been traded
+     * 0x0005 = The player has no deposited pokemon
+     * pkmn = If the Pokemon has been traded it returns the new Pokemon (100 bytes)
      */
     #[Route("/pokemonrse/worldexchange/result", name: "gts_result")]
-    public function result()
+    public function result(MA_Helper $helper)
     {
-        doAuth();
+        $helper->doAuth();
 
         $result=check_result();
 
@@ -54,12 +59,174 @@ class GTSController extends AbstractController
     }
 
     /**
-     * @Route("/worldexchange/get", name="gts_get")
+     * Retrieves the deposited Pokemon
      */
     #[Route("/pokemonrse/worldexchange/get", name: "gts_get")]
-    public function get_result()
+    public function get_result(MA_Helper $helper)
     {
-        doAuth();
+        $helper->doAuth();
+
+        $result=get_deposited();
+
+        if($result!=1){
+            return new Response($ar,Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        $ar = pack('n', $result);
+        return new Response($ar,Response::HTTP_OK,["content-length" => strlen($result)]);
+    }
+
+    /**
+     * When a traded Pokemon has been retreived delete it from the server
+     */
+    #[Route("/pokemonrse/worldexchange/delete", name: "gts_get")]
+    public function delete_result(MA_Helper $helper)
+    {
+        $helper->doAuth();
+
+        $result=get_deposited();
+
+        if($result!=1){
+            return new Response($ar,Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        $ar = pack('n', $result);
+        return new Response($ar,Response::HTTP_OK,["content-length" => strlen($result)]);
+    }
+
+    /**
+     * When a deposited Pokemon has been retreived delete it from the server
+     */
+    #[Route("/pokemonrse/worldexchange/return", name: "gts_get")]
+    public function return_result(MA_Helper $helper, GTS_Helper $GTS)
+    {
+        $helper->doAuth();
+
+        $result=$GTS->remove_pokemon();
+
+        if($result!=1){
+            return new Response($ar,Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        $ar = pack('n', $result);
+        return new Response($ar,Response::HTTP_OK,["content-length" => strlen($result)]);
+    }
+
+    /**
+     * Deposited a Pokemon into the GTS
+     */
+    #[Route("/pokemonrse/worldexchange/post", name: "gts_get")]
+    public function post_pokemon(MA_Helper $helper, PokemonGTSRepository $pokemon)
+    {
+        $helper->doAuth();
+
+        $result=get_deposited();
+
+
+
+        $pokemon = $pokemon->find($_GET['pid']);
+
+        if($pokemon==NULL){
+            return new Response('',Response::HTTP_UNAUTHORIZED);
+        }
+
+
+        $pokemonData = $helper->decrypt_data();
+
+        if($pokemon->getTrainerID!=substr($pokemonData,10,4)){
+            return new Response('',Response::HTTP_UNAUTHORIZED);
+        }
+
+        //Check Version (only emerald at the moment)
+        if(substr($pokemonData,0,1) != 0x03){
+            return new Response('',Response::HTTP_UNAUTHORIZED);
+        }
+        //Check Language (only english at the moment)
+        if(substr($pokemonData,7,1) != 0x02){
+            return new Response('',Response::HTTP_UNAUTHORIZED);
+        }
+
+        $pokemon->setVersion(substr($pokemonData,0,1));
+        $pokemon->setRomHackID(substr($pokemonData,1,4));
+        $pokemon->setRomHackVer(substr($pokemonData,5,2));
+        $pokemon->setLanguage(substr($profileData,7,1));
+
+        //Entity manager
+        $em = $this->getDoctrine()->getManager();
+        //Add to db
+        $em->persist($pokemon);
+        $em->flush();
+
+
+        if($result!=1){
+            return new Response($ar,Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        $ar = pack('n', $result);
+        return new Response($ar,Response::HTTP_OK,["content-length" => strlen($result)]);
+    }
+
+    /**
+     * Confirm Pokemon has been deposited during saving
+     */
+    #[Route("/pokemonrse/worldexchange/post_finish", name: "gts_get")]
+    public function post_finish(MA_Helper $helper, GTS_Helper $GTS)
+    {
+        $helper->doAuth();
+
+        $result=$GTS->remove_pokemon();
+
+        if($result!=1){
+            return new Response($ar,Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        $ar = pack('n', $result);
+        return new Response($ar,Response::HTTP_OK,["content-length" => strlen($result)]);
+    }
+
+    /**
+     * Search for Pokemon in the GTS
+     */
+    #[Route("/pokemonrse/worldexchange/search", name: "gts_get")]
+    public function search_pokemon(MA_Helper $helper)
+    {
+        $helper->doAuth();
+
+        $result=get_deposited();
+
+        if($result!=1){
+            return new Response($ar,Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        $ar = pack('n', $result);
+        return new Response($ar,Response::HTTP_OK,["content-length" => strlen($result)]);
+    }
+
+    /**
+     * Attempt to trade a Pokemon
+     */
+    #[Route("/pokemonrse/worldexchange/exchange", name: "gts_get")]
+    public function exchange_pokemon(MA_Helper $helper)
+    {
+        $helper->doAuth();
+
+        $result=get_deposited();
+
+        if($result!=1){
+            return new Response($ar,Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        $ar = pack('n', $result);
+        return new Response($ar,Response::HTTP_OK,["content-length" => strlen($result)]);
+    }
+
+    /**
+     * Confirm Pokemon has been traded during saving
+     */
+    #[Route("/pokemonrse/worldexchange/exchange_finish", name: "gts_get")]
+    public function exchange_finish(MA_Helper $helper)
+    {
+        $helper->doAuth();
 
         $result=get_deposited();
 
---
+++ src/Service/GTS_Helper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+class GTS_Helper
+{
+    // Search for pokemon in the database
+    public function query_pokemon(): string
+    {
+        
+    }
+
+    // Delete pokemon from the database
+    public function remove_pokemon(): string
+    {
+        
+    }
+}
